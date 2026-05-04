e99bff145ed5465879e5823caf0776a6f19dd70b
Patrick Morf <pat@patricks-macbook-pro.home>
2026-05-04 21:15:03 +0200

    name change

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // GitHub Pages: replace 'lade-tracker' with your repo name
-  base: '/lade-tracker/',
+  base: '/evChargeTracker/',
 })
